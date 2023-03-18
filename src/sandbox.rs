9053f12f513cca434f5406ee0600af370f0416f4
Can Ivit <canivit@users.noreply.github.com>
2023-03-18 11:33:09 -0400

    refactor sandbox error names

--- src/sandbox.rs
+++ src/sandbox.rs
@@ -20,12 +20,12 @@ pub enum Language {
 }
 
 #[derive(Error, Debug)]
-pub enum Error {
+pub enum SandboxError {
     #[error("directory {0:?} does not contain a dockerfile named 'Dockerfile'")]
     MissingDockerfile(PathBuf),
 
     #[error("failed to retrieve the absolute path of directory {directory:?}")]
-    FailedToRetrieveAbsolutePath {
+    RetrieveAbsolutePath {
         directory: PathBuf,
 
         #[source]
@@ -33,13 +33,13 @@ pub enum Error {
     },
 
     #[error("failed to create temp directory")]
-    FailedToCreateTempDirectory(#[source] std::io::Error),
+    CreateTempDirectory(#[source] std::io::Error),
 
     #[error("path {0:?} does not point to an existing file")]
     InvalidCodeFile(PathBuf),
 
     #[error("failed to copy the code file at {src:?} to {dest:?}")]
-    FailedToCopyCodeFile {
+    CopyCodeFile {
         src: PathBuf,
         dest: PathBuf,
 
@@ -48,10 +48,10 @@ pub enum Error {
     },
 
     #[error("failed to build docker image")]
-    FailedToBuildImage(#[source] shiplift::Error),
+    BuildImage(#[source] shiplift::Error),
 
     #[error("failed to create docker container from image with tag {image_tag:?}")]
-    FailedToCreateContainer {
+    CreateContainer {
         image_tag: String,
 
         #[source]
@@ -59,7 +59,7 @@ pub enum Error {
     },
 
     #[error("failed to attach to docker container with id {container_id:?}")]
-    FailedToAtachToContainer {
+    AtachToContainer {
         container_id: String,
 
         #[source]
@@ -67,7 +67,7 @@ pub enum Error {
     },
 
     #[error("failed to start docker container with id {container_id:?}")]
-    FailedToStartContainer {
+    StartContainer {
         container_id: String,
 
         #[source]
@@ -75,7 +75,7 @@ pub enum Error {
     },
 
     #[error("failed to execute {cmd:?} inside docker container")]
-    FailedToExecute {
+    Execute {
         cmd: String,
 
         #[source]
@@ -83,7 +83,7 @@ pub enum Error {
     },
 
     #[error("failed to remove docker container with id {container_id:?}")]
-    FailedToRemoveContainer {
+    RemoveContainer {
         container_id: String,
 
         #[source]
@@ -115,7 +115,7 @@ pub struct RunOutput {
 }
 
 impl DockerSandbox {
-    pub async fn new<T>(directory: T, image_tag: &str) -> Result<Self, Error>
+    pub async fn new<T>(directory: T, image_tag: &str) -> Result<Self, SandboxError>
     where
         T: AsRef<Path>,
     {
@@ -133,11 +133,11 @@ impl DockerSandbox {
         code_file: T,
         lang: Language,
         stdin: Option<&str>,
-    ) -> Result<RunOutput, Error>
+    ) -> Result<RunOutput, SandboxError>
     where
         T: AsRef<Path>,
     {
-        let temp_dir = TempDir::new("").map_err(Error::FailedToCreateTempDirectory)?;
+        let temp_dir = TempDir::new("").map_err(SandboxError::CreateTempDirectory)?;
         let sandbox_files = get_sandbox_files(code_file.as_ref(), lang, temp_dir.as_ref())?;
         let commands = get_commands(&sandbox_files, lang);
         copy_code_file(code_file.as_ref(), &sandbox_files.host_src)?;
@@ -163,29 +163,29 @@ impl DockerSandbox {
     }
 }
 
-fn validate_directory(dir: &Path) -> Result<PathBuf, Error> {
+fn validate_directory(dir: &Path) -> Result<PathBuf, SandboxError> {
     let docker_file = dir.join("Dockerfile");
     let exist = docker_file
         .try_exists()
-        .map_err(|_err| Error::MissingDockerfile(dir.to_path_buf()))?;
+        .map_err(|_err| SandboxError::MissingDockerfile(dir.to_path_buf()))?;
     if !exist || !docker_file.is_file() {
-        return Err(Error::MissingDockerfile(dir.to_path_buf()));
+        return Err(SandboxError::MissingDockerfile(dir.to_path_buf()));
     }
 
     dir.canonicalize()
-        .map_err(|err| Error::FailedToRetrieveAbsolutePath {
+        .map_err(|err| SandboxError::RetrieveAbsolutePath {
             directory: dir.to_path_buf(),
             source: err,
         })
 }
 
-async fn build_image(docker: &Docker, path: &Path, tag: &str) -> Result<(), Error> {
+async fn build_image(docker: &Docker, path: &Path, tag: &str) -> Result<(), SandboxError> {
     let options = BuildOptions::builder(path.display().to_string())
         .tag(tag)
         .build();
     let mut stream = docker.images().build(&options);
     while let Some(build_result) = stream.next().await {
-        build_result.map_err(Error::FailedToBuildImage)?;
+        build_result.map_err(SandboxError::BuildImage)?;
     }
     Ok(())
 }
@@ -196,14 +196,14 @@ async fn exec_container(
     image_tag: &str,
     cmd: &[String],
     stdin: Option<&str>,
-) -> Result<RunOutput, Error> {
+) -> Result<RunOutput, SandboxError> {
     let container_id = create_container(docker, temp_dir, image_tag, cmd).await?;
     let container = docker.containers().get(&container_id);
 
     container
         .start()
         .await
-        .map_err(|err| Error::FailedToStartContainer {
+        .map_err(|err| SandboxError::StartContainer {
             container_id: container_id.to_owned(),
             source: err,
         })?;
@@ -211,7 +211,7 @@ async fn exec_container(
     let (mut reader, mut writer) = container
         .attach()
         .await
-        .map_err(|err| Error::FailedToAtachToContainer {
+        .map_err(|err| SandboxError::AtachToContainer {
             container_id: container_id.to_owned(),
             source: err,
         })?
@@ -221,15 +221,15 @@ async fn exec_container(
         writer
             .write_all(s.as_bytes())
             .await
-            .map_err(Error::WriteToStdin)?;
-        writer.flush().await.map_err(Error::WriteToStdin)?;
+            .map_err(SandboxError::WriteToStdin)?;
+        writer.flush().await.map_err(SandboxError::WriteToStdin)?;
     }
     //writer.close().await.map_err(Error::CloseStdin)?;
 
     let mut stdout: Vec<u8> = Vec::new();
     let mut stderr: Vec<u8> = Vec::new();
     while let Some(result) = reader.next().await {
-        let chunk = result.map_err(|err| Error::FailedToExecute {
+        let chunk = result.map_err(|err| SandboxError::Execute {
             cmd: cmd.join(" "),
             source: err,
         })?;
@@ -244,11 +244,11 @@ async fn exec_container(
     remove_container(&container).await?;
 
     let stdout = from_utf8(&stdout)
-        .map_err(|err| Error::InvalidBytesStdOut { source: err })?
+        .map_err(|err| SandboxError::InvalidBytesStdOut { source: err })?
         .to_owned();
 
     let stderr = from_utf8(&stderr)
-        .map_err(|err| Error::InvalidBytesStdErr { source: err })?
+        .map_err(|err| SandboxError::InvalidBytesStdErr { source: err })?
         .to_owned();
 
     Ok(RunOutput { stdout, stderr })
@@ -259,7 +259,7 @@ async fn create_container(
     temp_dir: &Path,
     image_tag: &str,
     cmd: &[String],
-) -> Result<String, Error> {
+) -> Result<String, SandboxError> {
     let mount = format!("{}:/home/sandbox", temp_dir.display());
     let slice_cmd: Vec<&str> = cmd.iter().map(String::as_str).collect();
     let options = ContainerOptions::builder(image_tag)
@@ -272,7 +272,7 @@ async fn create_container(
         .build();
     docker.containers().create(&options).await.map_or_else(
         |err| {
-            Err(Error::FailedToCreateContainer {
+            Err(SandboxError::CreateContainer {
                 image_tag: image_tag.to_owned(),
                 source: err,
             })
@@ -281,7 +281,7 @@ async fn create_container(
     )
 }
 
-async fn remove_container(container: &Container<'_>) -> Result<(), Error> {
+async fn remove_container(container: &Container<'_>) -> Result<(), SandboxError> {
     let options = RmContainerOptions::builder()
         .volumes(true)
         .force(true)
@@ -289,7 +289,7 @@ async fn remove_container(container: &Container<'_>) -> Result<(), Error> {
     container
         .remove(options)
         .await
-        .map_err(|err| Error::FailedToRemoveContainer {
+        .map_err(|err| SandboxError::RemoveContainer {
             container_id: container.id().to_owned(),
             source: err,
         })?;
@@ -335,8 +335,8 @@ fn get_runner(lang: Language) -> &'static str {
     }
 }
 
-fn copy_code_file(src: &Path, dest: &Path) -> Result<(), Error> {
-    fs::copy(src, dest).map_err(|err| Error::FailedToCopyCodeFile {
+fn copy_code_file(src: &Path, dest: &Path) -> Result<(), SandboxError> {
+    fs::copy(src, dest).map_err(|err| SandboxError::CopyCodeFile {
         src: src.to_path_buf(),
         dest: dest.to_path_buf(),
         source: err,
@@ -348,10 +348,10 @@ fn get_sandbox_files(
     code_file: &Path,
     lang: Language,
     temp_dir: &Path,
-) -> Result<SandboxFiles, Error> {
+) -> Result<SandboxFiles, SandboxError> {
     let base_file_name: PathBuf = code_file
         .file_stem()
-        .ok_or_else(|| Error::InvalidCodeFile(code_file.to_path_buf()))?
+        .ok_or_else(|| SandboxError::InvalidCodeFile(code_file.to_path_buf()))?
         .into();
     let source_ext = get_source_extension(lang);
     let compiled_ext = get_compiled_extension(lang);
