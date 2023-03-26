d087d6686fdf2502c21f3bcb8e06ca68006b2b93
Can Ivit <canivit@users.noreply.github.com>
2023-03-26 09:06:08 -0400

    progress on implementing workflow execute

--- src/workflow.rs
+++ src/workflow.rs
@@ -2,6 +2,7 @@ use std::path::{Path, PathBuf};
 use std::time::Duration;
 
 use thiserror::Error;
+use tokio::time::Instant;
 
 pub use crate::sandbox::Language;
 use crate::sandbox::{DockerSandbox, SandboxError};
@@ -17,6 +18,7 @@ pub struct Workflow {
 pub struct Step {
     pub lang: Language,
     pub code_file: PathBuf,
+    pub timeout: Duration,
     pub desc: String,
 }
 
@@ -56,9 +58,8 @@ pub struct ExportResult {
 }
 
 pub struct WorkflowResult {
-    pub output: String,
-    pub step_results: Vec<StepResult>,
-    pub export_results: Vec<ExportResult>,
+    step_results: Vec<StepResult>,
+    export_results: Vec<ExportResult>,
 }
 
 #[derive(Error, Debug)]
@@ -94,6 +95,61 @@ impl Workflow {
             steps: Vec::new(),
             exports: Vec::new(),
         }
+    }
+
+    pub fn input(&self) -> &str {
+        &self.input
+    }
+
+    pub fn steps(&self) -> impl Iterator<Item = &Step> {
+        self.steps.iter()
+    }
+
+    pub fn exports(&self) -> impl Iterator<Item = &Export> {
+        self.exports.iter()
+    }
+
+    pub async fn execute(&self) -> Result<WorkflowResult, WorkflowError> {
+        let mut step_results = Vec::<StepResult>::new();
+        for (idx, step) in self.steps.iter().enumerate() {
+            let input = step_results
+                .last()
+                .map_or(&self.input, |last_result| &last_result.stdout);
+            match step.execute(input, idx, &self.sandbox).await {
+                Ok(r) => step_results.push(r),
+                Err(err) => {
+                    return Err(WorkflowError::StepError {
+                        source: err,
+                        prev_steps_results: step_results,
+                    })
+                }
+            };
+        }
+        Ok(WorkflowResult {
+            step_results,
+            export_results: Vec::new(),
+        })
+    }
+}
+
+impl Step {
+    async fn execute(
+        &self,
+        input: &str,
+        step_idx: usize,
+        sandbox: &DockerSandbox,
+    ) -> Result<StepResult, SandboxError> {
+        let start = Instant::now();
+        let output = sandbox
+            .run_code(&self.code_file, self.lang, self.timeout, Some(input))
+            .await?;
+        let exec_time = start.elapsed();
+        Ok(StepResult {
+            step_idx,
+            stdout: output.stdout,
+            stderr: output.stderr,
+            exec_time,
+        })
     }
 }
 
